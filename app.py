fffe5223e9fddfe556152f34f39901d79bdd92c0
ekumamait <ericebuluochol@gmail.com>
2018-07-26 16:43:35 +0300

    update app_py file

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request  
+from datetime import datetime
 
 
 app = Flask(__name__)
+date = datetime.now()
+now = date.strftime("%d-%m-%Y %H:%M")
 
-Entries = [
-{"entry_id": 1, "date": "01/11/2018", "title": "first safari","description": "advkbfsdkjbv"},
-{"entry_id": 2, "date": "01/11/2018", "title": "first safari","description": "advkbfsdkjbv"},
-{"entry_id": 3, "date": "01/11/2018", "title": "first safari","description": "advkbfsdkjbv"}]
+Entries = []
 
 @app.route('/')
 def index():
@@ -14,7 +14,7 @@ def index():
 
 @app.route('/api/v1/entries', methods = ['GET'] )
 def all_entries():
-	return jsonify({'entries': Entries})
+	return jsonify(Entries)
 
 @app.route('/api/v1/entries/<int:entry_id>', methods = ['GET'] )
 def single_entry(entry_id):
@@ -24,24 +24,30 @@ def single_entry(entry_id):
 
 @app.route('/api/v1/entries', methods = ['POST'] )
 def add_entry():
-
-	new_entry = dict(entry_id = request.json['entry_id'], date=request.json['date'], title=request.json['title'], description=request.json['description']) 
+	new_entry = request.get_json() 
+	new_entry['date'] = now
 	Entries.append(new_entry)
+	id = 1
+	for entry in Entries:
+		entry['entry_id'] = id
+		id += 1
 	return jsonify({'entries': Entries})
 
 @app.route('/api/v1/entries/<int:entry_id>', methods = ['Delete'] )
-def delete_entry(entry_id):
-
-	entry = [entry for entry in Entries if entry['entry_id']==entry_id]
-	Entries.remove(entry[0])
-	return jsonify({'entries': Entries})	
+def delete_entry(entry_id):	
+	for entry in Entries:
+		if entry['entry_id'] == entry_id:
+			Entries.remove(entry)	
+			return jsonify({'200': 'Entry removed'})	
 
 @app.route('/api/v1/entries/<int:entry_id>', methods = ['PUT'] )
 def edit_entry(entry_id):
-
+	new_entry = request.get_json()
 	entry = [entry for entry in Entries if entry['entry_id']==entry_id]
-	Entries[0]['entry_id']= request.json['entry_id']
-	return jsonify(dict(entry=entry[0]))			
+	for entry in Entries:
+		if entry['entry_id'] == entry_id:
+			entry['content'] = new_entry['content']
+	return jsonify({'200' : 'Entry updated'})			
 
 if __name__=='__main__':
 	app.run(debug=True)	
